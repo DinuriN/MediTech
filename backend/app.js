cc3a4e4a56836c533a8fb21a29540164ecc1f209
DinuriN <dinurin730@gmail.com>
2025-03-17 13:16:32 +0530

    Revert "Merge pull request #2 from DinuriN/frontend-components"
    This reverts commit b842e615fad9fbb062684920ceeaa445562563c8, reversing
    changes made to 4c3f2e93f2e20f9011dffc60ef534cb978715444.

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,13 @@
 //TyslDJir7WM2Up5u
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
-const registerPatientRouter = require("./Routes/register-patient-route");
 
 const app = express();
 
 //Middleware
-// app.use("/", (req, res, next)=>{
-//     res.send("Working...");
-// })
-app.use(express.json());
-app.use(cors());
-app.use("/patients", registerPatientRouter)
-
+app.use("/", (req, res, next)=>{
+    res.send("Working...");
+})
 
 //Connecting mongodb
 mongoose.connect("mongodb+srv://admin:TyslDJir7WM2Up5u@meditech-cluster.jf2kb.mongodb.net/")
