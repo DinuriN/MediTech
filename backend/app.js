4bc9a8f053f94c20ae9ee2ec8da6976268b9e4b0
DinuriN <dinurin730@gmail.com>
2025-03-17 13:39:40 +0530

    commit-patient-reg

--- backend/app.js
+++ backend/app.js
@@ -7,12 +7,9 @@ const registerPatientRouter = require("./Routes/register-patient-route");
 const app = express();
 
 //Middleware
-// app.use("/", (req, res, next)=>{
-//     res.send("Working...");
-// })
-app.use(express.json());
-app.use(cors());
-app.use("/patients", registerPatientRouter)
+ app.use("/", (req, res, next)=>{
+     res.send("Working...");
+ })
 
 
 //Connecting mongodb
