95fae27452d0aa245ee2e7d374798563085f5533
DinuriN <dinurin730@gmail.com>
2025-03-17 13:05:59 +0530

    frontend components folder added

---
+++ backend/Models/patient-registration-model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const registerPatientSchema = new Schema({
+    patientId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    contactNo: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model(
+    "patient-registration-model",
+    registerPatientSchema
+)
