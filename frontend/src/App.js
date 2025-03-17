b78213e30dbd1a4220c286225fcd21ebbc164708
DinuriN <dinurin730@gmail.com>
2025-03-17 14:20:22 +0530

    navbar added - frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+
+import {Route, Routes} from "react-router";
+import NavBar from './Components/Common/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Frontend - working</h1>
+      <React.Fragment>
+        <Routes>
+          <Route path="/" element={<NavBar/>} />
+        </Routes>
+      </React.Fragment>
+
     </div>
   );
 }
