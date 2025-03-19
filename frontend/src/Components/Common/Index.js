d11ae5f0ef2ddb01041c2ba726bc82167dae191d
DinuriN <dinurin730@gmail.com>
2025-03-19 22:40:32 +0530

    home header, footer:draft v3

--- frontend/src/Components/Common/Index.js
+++ frontend/src/Components/Common/Index.js
@@ -13,14 +13,23 @@ function Index() {
         <div className="meditech-banner-container">
           <img src={meditechBanner} alt="meditech-banner-image" />
         </div>
+
+        <div className="doctor-appointment-button">
+            <button><i class='bx bx-calendar-plus'></i>  Appointment</button>
+        </div>
+
+        <div className="bg-img-content-1">
+            <h1>WE ARE MEDITECH</h1>
+            <p>C.R. Meditech Lab (PVT) LTD was founded to provide advanced healthcare 
+                at affordable rates. Backed by a team of expert consultants, physicians, 
+                and state-of-the-art technology, we offer comprehensive medical services under one roof.</p>
+        </div>
       </div>
 
       <div className="index-content">
-        <p>aaa</p>
-        <p>aaa</p>
-        <p>aaa</p>
-        <p>aaa</p>
-        <p>aaa</p>
+        <div className="index-content-col-1"></div>
+        
+        <div className="index-content-col-2"></div>
       </div>
 
       <div className="footer-container-home">
