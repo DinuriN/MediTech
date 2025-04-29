47d46d9f70544fb430d36fdaeb36c29dedd24944
arosha112432 <ar112432@fhss.sjp.ac.lk>
2025-04-29 23:44:38 +0530

    update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,11 +13,9 @@ const chatbotRoutes = require('./Routes/chatbot-route');
 app.use(express.json());
 app.use(cors());
 app.use("/labEquipments", routerLabEqipment);
-<<<<<<< HEAD
+
 app.use('/api/chatbot', chatbotRoutes);
 
-=======
->>>>>>> f560ee5c4d61bac79238952a0876d657c0059e68
 
 //Connecting mongodb
 mongoose.connect("mongodb+srv://admin:TyslDJir7WM2Up5u@meditech-cluster.jf2kb.mongodb.net/")
